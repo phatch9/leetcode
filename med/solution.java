80b75293230190e52f9deaf3fb2e5315e4c9f225
Phat <phat.chauu26@gmail.com>
2024-08-26 18:50:24 -0700

    problem 3 checked

--- med/solution.java
+++ med/solution.java
@@ -28,7 +28,7 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces */
 
-public class Longest_Substring {
+public class solution {
 
         public int lengthOfLongestSubstring(String s) {
             int n = s.length();
